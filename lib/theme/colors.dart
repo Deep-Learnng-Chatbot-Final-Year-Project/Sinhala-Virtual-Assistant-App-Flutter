d7f50df1e8ed1d13a8507a0dddd8492d0f59b028
madushan <ranasinhemadushan@gmail.com>
2023-05-27 23:37:46 +0530

    init

---
+++ lib/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class ColorPack {
+  static const Color primaryColor = Color(0xFF0C5F4B);
+
+}
---
+++ lib/utills/appConstant.dart
@@ -0,0 +1,7 @@
+class AppConstant {
+  static String chatApi = "https://chat.enzy.live/chat/ask";
+  // {"text": " ඔයා කවුද"}
+
+
+
+}
