9e00a5a0c1750803e959280e4025083ced8483e2
madushan <ranasinhemadushan@gmail.com>
2023-05-28 13:09:32 +0530

    assets and colors added

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,5 +2,7 @@ import 'dart:ui';
 
 class ColorPack {
   static const Color primaryColor = Color(0xFF0C5F4B);
+  static const Color backgroundColor = Color(0xFF343541);
+  static const Color cardColor = Color(0xFF444654);
 
 }
