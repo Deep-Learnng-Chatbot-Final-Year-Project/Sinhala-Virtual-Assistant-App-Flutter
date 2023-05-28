e82811987f42c5559467e150fc44ea050d8535c4
madushan <ranasinhemadushan@gmail.com>
2023-05-28 16:03:49 +0530

    chat service,controller

---
+++ lib/controller/chatController.dart
@@ -0,0 +1,18 @@
+import 'package:ai_assistant/model/chatResponse.dart';
+import 'package:get/get.dart';
+
+import '../services/chatService.dart';
+
+class ChatController extends GetxController {
+  final ChatService chatService = ChatService();
+  var chatList = <ChatResponse>[].obs;
+
+  sendMessage(String value) async {
+    await chatService.getChat(value).then((response) {
+      if (response != null) {
+        chatList.add(response);
+      }
+      print(chatList);
+    });
+  }
+}
---
+++ lib/services/chatService.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:ai_assistant/model/chatRequest.dart';
+import 'package:ai_assistant/model/chatResponse.dart';
+import 'package:ai_assistant/utills/appConstant.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class ChatService {
+  Future<ChatResponse?> getChat(String question) async {
+    try {
+      var response = await http.post(
+        Uri.parse(AppConstant.chatApiAsk),
+        headers: {
+          "Accept": "application/json",
+          "content-type": "application/json"
+        },
+        body: json.encode(ChatRequest(text: question).toJson()),
+      );
+      if (response.statusCode == 200) {
+        ChatResponse chatResponse =
+            ChatResponse.fromJson(jsonDecode(response.body));
+        return chatResponse;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
+  }
+}
